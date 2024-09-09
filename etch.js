4b0d80032992773881197cc924bcde4591a93da6
Emmanuel Cruz Real <ecruzrea@uci.edu>
2024-09-09 14:25:47 -0700

    Implemented a button to change the size of a grid given a length of the side n

--- etch.js
+++ etch.js
@@ -1,6 +1,7 @@
 const container  = document.querySelector('.container')
 
 function constructGrid(size=16){
+    container.innerHTML = '';
     const box_size = 500 / size;
     const num_boxes = size * size;
     for (let i = 1; i <= num_boxes; i++){
@@ -16,6 +17,7 @@ constructGrid()
 const draw = document.querySelector('#draw')
 const erase = document.querySelector('#erase')
 const reset  = document.querySelector('#reset')
+const update_grid = document.querySelector('#grid-update')
 
 draw.addEventListener('click', () => {
     const grid = document.querySelectorAll('.box')
@@ -36,4 +38,13 @@ reset.addEventListener('click', () => {
     grid.forEach(box => {
         box.style.backgroundColor = 'white';
     })
+})
+
+update_grid.addEventListener('click', () => {
+    let new_num = prompt('Please enter a number between 2 and 99')
+    if (new_num >= 2 && new_num <= 99){
+        constructGrid(new_num)
+    } else {
+        alert('Only integers between 2 and 99 are supported.')
+    }
 })
